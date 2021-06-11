ce5608d246976abe8fdbfc777af959cb6536aec5
KodeMafia <dharmendramehta622@gmail.com>
2021-06-12 02:49:21 +0545

    uri error solved for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:syllabuspu/screens/home_screen.dart';
-import 'package:syllabuspu/screens/login_screen.dart';
-import 'package:syllabuspu/screens/spash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/spash_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
